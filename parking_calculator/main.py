7f12ef06380304cbce18c7899cf77d72c67d3154
Küzmös Áron <kuzmos.aron@gmail.com>
2026-04-19 13:11:42 +0200

    Modified parking claculator display

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -2,13 +2,10 @@ from pathlib import Path
 from datetime import datetime
 import math
 
-
 def parsing(arrival, departure):
     format_string = "%Y-%m-%d %H:%M:%S"
-
     d1 = datetime.strptime(arrival, format_string)
     d2 = datetime.strptime(departure, format_string)
-
     if d1 > d2:
         return
     
@@ -17,14 +14,12 @@ def parsing(arrival, departure):
 
 def FeeCalculation(period):
     fee = 0
-
     if period <= 30:
         return 0
     
     days = period // 1440
     restMinutes = period % 1440
     fee += days * 10000
-
     if restMinutes >= 30:
         restMinutes -= 30
     else:
@@ -45,36 +40,33 @@ def main():
     if not file_path.exists():
         print("Hiba: Az input.txt fájl nem található!")
         return
-
     
     content = file_path.read_text(encoding="utf-8").splitlines()
     data_lines = content[2:]
-
     results = []
-
+    print(f"Rendszam \t DIJ")
     for line in data_lines:
         if not line.strip():
             continue
-            
-        
-        parts = line.split()
-        
-        if len(parts) == 5:
-            licencePlate = parts[0]
-            arrival = f"{parts[1]} {parts[2]}"
-            departure = f"{parts[3]} {parts[4]}"
-            
-            
-            minutes = parsing(arrival, departure)
-            
-            if minutes is not None:
-                
-                fee = FeeCalculation(minutes)
-                results.append(f"{licencePlate}: {fee} Ft")
-                print(f"{licencePlate} -> {minutes:.1f} perc -> {fee} Ft")
-            else:
-                results.append(f"{licencePlate}: HIBA (Időrend)")
 
+        parts = line.split("\t\t")
+
+        if len(parts) != 3:
+            print(f"invalid data: {line}")
+            continue
+
+        license_plate = parts[0].strip()
+        arrival = parts[1].strip()
+        departure = parts[2].strip()
+
+        minutes = parsing(arrival, departure)
+        
+        if minutes is not None:
+            fee = FeeCalculation(minutes)
+            results.append(f"{license_plate}: {fee} Ft")
+            print(f"{license_plate} \t {fee}")
+        else:
+            results.append(f"{license_plate}: HIBA (Időrend)")
     
     Path("parking_results.txt").write_text("\n".join(results), encoding="utf-8")
     print("\nAz eredmények elmentve a parking_results.txt fájlba.")
